bcaf9c2ef95f4f6244d5d18d699a37512026da4a
whysoezzy <whysoezzy@gmail.com>
2026-06-08 16:54:33 +0300

    chore(config): add ingestion and admin properties

---
+++ src/main/kotlin/dev/whysoezzy/meet/config/SchedulingConfig.kt
@@ -0,0 +1,8 @@
+package dev.whysoezzy.meet.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class SchedulingConfig
