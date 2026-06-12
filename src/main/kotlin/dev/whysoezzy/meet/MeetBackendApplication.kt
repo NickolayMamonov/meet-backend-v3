5a9b9158dd137b23b60c955409cb86895b26a490
whysoezzy <whysoezzy@gmail.com>
2026-06-12 16:00:38 +0300

    fix(config): register Geocoder/StaticMap properties via @EnableConfigurationProperties

--- src/main/kotlin/dev/whysoezzy/meet/MeetBackendApplication.kt
+++ src/main/kotlin/dev/whysoezzy/meet/MeetBackendApplication.kt
@@ -1,9 +1,13 @@
 package dev.whysoezzy.meet
 
+import dev.whysoezzy.meet.config.GeocoderProperties
+import dev.whysoezzy.meet.config.StaticMapProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(GeocoderProperties::class, StaticMapProperties::class)
 class MeetBackendApplication
 
 fun main(args: Array<String>) {
